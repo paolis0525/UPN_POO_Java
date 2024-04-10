905392bb9849400dad547152324293cbe78f596b
Paito <pbellido08@gmail.com>
2024-04-09 21:52:21 -0500

    aqui quede
    get set

--- Pao/Paola/src/main/java/com/mycompany/paola/Persona.java
+++ Pao/Paola/src/main/java/com/mycompany/paola/Persona.java
@@ -9,5 +9,43 @@ package com.mycompany.paola;
  * @author pao
  */
 public class Persona {
+    int edad;
+    String sexo;
+    String nombre;
+
+    public Persona() {
+    }
+
+    public Persona(int edad, String sexo, String nombre) {
+        this.edad = edad;
+        this.sexo = sexo;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
     
 }
