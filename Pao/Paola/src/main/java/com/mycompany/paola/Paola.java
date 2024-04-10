26927d3e1e52ff229895024ffa671186311ed389
Paito <pbellido08@gmail.com>
2024-04-09 21:44:50 -0500

    crear un proyecto

---
+++ Pao/Paola/src/main/java/com/mycompany/paola/Paola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.paola;
+
+/**
+ *
+ * @author pao
+ */
+public class Paola {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
