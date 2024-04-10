4e1bbb50caca3b03a813ecc5bd39b9237f1fffa9
Paito <pbellido08@gmail.com>
2024-04-09 22:18:01 -0500

    for each
    loop para recorrer array

--- Pao/Paola/src/main/java/com/mycompany/paola/Paola.java
+++ Pao/Paola/src/main/java/com/mycompany/paola/Paola.java
@@ -11,6 +11,17 @@ package com.mycompany.paola;
 public class Paola {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hel");
+        
+        Persona person[] = new Persona[3];
+        
+        person[0] = new Persona(20, "Spfia", "1111111"); 
+        person[1] = new Persona(20, "Spfia", "2222222");
+        person[2] = new Persona(20, "Spfia", "3333333");
+
+                
+        for (Persona item : person) {
+            System.out.println("Mi nombre es: " + item.getNombre());
+        }
     }
 }
